ce249f96eadd81ed48b00d754831b6676c6bfab9
Michael Rashkovetskyi <misharash@gmail.com>
2022-05-23 20:25:53 -0400

    Set intermediate number of loops

--- modules/parameters.h
+++ modules/parameters.h
@@ -80,7 +80,7 @@ public:
     //---------- PRECISION PARAMETERS ---------------------------------------
 
     // Maximum number of iterations to compute the C_ab integrals over
-    int max_loops = 120;
+    int max_loops = 360;
 
     // Number of random cells to draw at each stage
     int N2 = 20; // number of j cells per i cell
