358874cefa6f27b429f76d8515b486baad5227aa
Michael Rashkovetskyi <misharash@gmail.com>
2022-05-30 17:15:54 -0400

    Do not exit on dependent catalogs suspicion

--- modules/integrals.h
+++ modules/integrals.h
@@ -246,9 +246,10 @@ public:
             tmp_weight = wij[i]*pk.w; // product of weights, w_iw_jw_k
 
             if(rik_mag<1e-4){
-              printf("Particle separation of %.2e Mpc/h found between random particle files %d and %d. This is unusually small and will cause errors.\n",rik_mag,I1,I3);
-              printf("Are the random particle files independent? The code will now exit.");
-              exit(1);
+              printf("Particle separation of %.2e Mpc/h found between random particles %d and %d from files %d and %d. This is unusually small and will cause errors.\n", rik_mag, prim_ids[i], pk_id, I1, I3);
+              printf("Are the random particle files independent? The code will skip this pair.");
+              wijk[i] = -1; // skip the problematic pair
+              continue;
             }
 
             // save arrays for later
