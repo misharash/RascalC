e20e383335aec92773080a9fb0e0e5469a22935a
Michael Rashkovetskyi <misharash@gmail.com>
2022-08-02 12:02:36 -0700

    Fixes, adjustments, config for EDAbeta LRG_N

--- run_cov.py
+++ run_cov.py
@@ -26,14 +26,14 @@ nbin_cf = 200 # radial bins for input 2PCF
 mbin_cf = 10 # angular (mu) bins for input 2PCF
 xicutoff = 250 # beyond this assume xi/2PCF=0
 
-nthread = 30 # number of OMP threads to use
+nthread = 1 # number of OMP threads to use
 maxloops = 60 # number of integration loops per filename
 N2 = 20 # number of secondary cells/particles per primary cell
 N3 = 40 # number of third cells/particles per secondary cell/particle
 N4 = 80 # number of fourth cells/particles per third cell/particle
 
 rescale = 1 # rescaling for co-ordinates
-nside = 101 # grid size for accelerating pair count
+ngrid = 101 # grid size for accelerating pair count
 boxsize = 2000 # only used if periodic=1
 
 # data processing steps
@@ -44,11 +44,11 @@ create_jackknives = jackknife and 1
 # CF options
 convert_cf = 1
 if convert_cf:
-    pycorr_filename = "allcounts_LRG_NScomb_0.4_1.1_default_FKP_lin_njack60_nran10.npy"
+    pycorr_filename = "/global/cfs/projectdirs/desi/survey/catalogs/DA02/LSS/guadalupe/LSScats/EDAbeta/xi/smu/allcounts_LRG_N_0.4_1.1_default_FKP_lin_njack60_nran10.npy"
     counts_factor = 10
 smoothen_cf = 1
 if smoothen_cf:
-    max_l = 8
+    max_l = 4
     radial_window_len = 5
     radial_polyorder = 2
 
@@ -61,8 +61,8 @@ if convert_to_xyz:
 z_min, z_max = 0.4, 1.1 # for redshift cut
 
 # File names and directories
-data_ref_filename = "LRG_N_clustering.dat.fits" # for jackknife reference only, has to have rdz contents
-input_filenames = ["LRG_N_0_clustering.ran.fits"] # random filenames
+data_ref_filename = "/global/cfs/projectdirs/desi/survey/catalogs/DA02/LSS/guadalupe/LSScats/EDAbeta/LRG_N_clustering.dat.fits" # for jackknife reference only, has to have rdz contents
+input_filenames = [f"/global/cfs/projectdirs/desi/survey/catalogs/DA02/LSS/guadalupe/LSScats/EDAbeta/LRG_N_{i}_clustering.ran.fits" for i in range(1)] # random filenames
 nfiles = len(input_filenames)
 corname = f"xi/xi_n{nbin}_m{mbin}_11.dat"
 binned_pair_name = f"weights/binned_pair_counts_n{nbin}_m{mbin}_j{njack}_11.dat"
@@ -72,24 +72,6 @@ if jackknife:
         xi_jack_name = f"xi_jack/xi_jack_n{nbin}_m{mbin}_j{njack}_11.dat"
         jackknife_pairs_name = f"weights/jackknife_pair_counts_n{nbin}_m{mbin}_j{njack}_11.dat"
 outdir = "out" # output file directory
-
-# CF conversion
-if convert_cf:
-    # full-survey CF
-    os.makedirs(os.path.dirname(corname), exist_ok=1) # make sure all dirs exist
-    r_step = (rmax_cf-rmin_cf)//nbin_cf
-    os.system(f"python python/convert_xi_from_pycorr.py {pycorr_filename} {corname} {r_step} {mbin_cf} | tee -a {logfile}")
-    if smoothen_cf:
-        corname_old = corname
-        corname = f"xi/xi_n{nbin}_m{mbin}_11_smooth.dat"
-        os.system(f"python python/smoothen_xi.py {corname_old} {max_l} {radial_window_len} {radial_polyorder} {corname} | tee -a {logfile}")
-    os.makedirs(os.path.dirname(binned_pair_name), exist_ok=1) # make sure all dirs exist
-    if jackknife: # convert jackknife xi and all counts
-        for filename in (xi_jack_name, jackknife_weights_name, jackknife_pairs_name):
-            os.makedirs(os.path.dirname(filename), exist_ok=1) # make sure all dirs exist
-        os.system(f"python python/convert_xi_jack_from_pycorr.py {pycorr_filename} {xi_jack_name} {jackknife_weights_name} {jackknife_pairs_name} {binned_pair_name} {r_step} {mbin_cf} {counts_factor} | tee -a {logfile}")
-    else: # only convert full, binned pair counts
-        os.system(f"python python/convert_xi_jack_from_pycorr.py {pycorr_filename} {binned_pair_name} {r_step} {mbin_cf} {counts_factor} | tee -a {logfile}")
 
 # binning files to be created automatically
 binfile = "radial_binning_cov.csv"
@@ -109,12 +91,12 @@ if jackknife:
 # Create output directory
 os.makedirs(outdir, exist_ok=1)
 
-# Copy this script in for posterity
-os.system(f"cp {__file__} {os.path.normpath(outdir)}")
-
 # Create an output file for errors
 logfilename = "log.txt"
 logfile = os.path.join(outdir, logfilename)
+
+# Copy this script in for posterity
+os.system(f"cp {__file__} {os.path.normpath(outdir)}")
 
 def print_and_log(s):
     print(s)
@@ -123,9 +105,31 @@ print_log = lambda l: os.system(f"echo \"{l}\" >> {logfile}")
 
 print_and_log(datetime.now())
 print_and_log(f"Executing {__file__}")
-print_and_log(command)
+print_and_log(f"Common command for C++ code: {command}")
+
+def exec_print_and_log(commandline):
+    print_and_log(f"Running command: {commandline}")
+    os.system(f"{commandline} | tee -a {logfile}")
 
 print("Starting Computation")
+
+# CF conversion
+if convert_cf:
+    # full-survey CF
+    os.makedirs(os.path.dirname(corname), exist_ok=1) # make sure all dirs exist
+    r_step = (rmax_cf-rmin_cf)//nbin_cf
+    exec_print_and_log(f"python python/convert_xi_from_pycorr.py {pycorr_filename} {corname} {r_step} {mbin_cf}")
+    if smoothen_cf:
+        corname_old = corname
+        corname = f"xi/xi_n{nbin}_m{mbin}_11_smooth.dat"
+        exec_print_and_log(f"python python/smoothen_xi.py {corname_old} {max_l} {radial_window_len} {radial_polyorder} {corname}")
+    os.makedirs(os.path.dirname(binned_pair_name), exist_ok=1) # make sure all dirs exist
+    if jackknife: # convert jackknife xi and all counts
+        for filename in (xi_jack_name, jackknife_weights_name, jackknife_pairs_name):
+            os.makedirs(os.path.dirname(filename), exist_ok=1) # make sure all dirs exist
+        exec_print_and_log(f"python python/convert_xi_jack_from_pycorr.py {pycorr_filename} {xi_jack_name} {jackknife_weights_name} {jackknife_pairs_name} {binned_pair_name} {r_step} {mbin_cf} {counts_factor}")
+    else: # only convert full, binned pair counts
+        exec_print_and_log(f"python python/convert_xi_jack_from_pycorr.py {pycorr_filename} {binned_pair_name} {r_step} {mbin_cf} {counts_factor}")
 
 if periodic and make_randoms:
     # create random points
@@ -142,7 +146,7 @@ def change_extension(name, ext):
 if create_jackknives and redshift_cut: # prepare reference file
     print_and_log(f"Processing data file for jackknife reference")
     rdzw_ref_filename = change_extension(data_ref_filename, "rdzw")
-    os.system(f"python python/redshift_cut.py {input_filename} {rdzw_filename} {z_min} {z_max} {FKP_weight} | tee -a {logfile}")
+    exec_print_and_log(f"python python/redshift_cut.py {data_ref_filename} {rdzw_ref_filename} {z_min} {z_max} {FKP_weight}")
     data_ref_filename = rdzw_ref_filename
 
 # for each random file/part
@@ -155,20 +159,20 @@ for i, input_filename in enumerate(input_filenames):
     else: # (potentially) run through all data processing steps
         if redshift_cut:
             rdzw_filename = change_extension(input_filename, "rdzw")
-            os.system(f"python python/redshift_cut.py {input_filename} {rdzw_filename} {z_min} {z_max} {FKP_weight} | tee -a {logfile}")
+            exec_print_and_log(f"python python/redshift_cut.py {input_filename} {rdzw_filename} {z_min} {z_max} {FKP_weight}")
             input_filename = rdzw_filename
         if convert_to_xyz:
             xyzw_filename = change_extension(input_filename, "xyzw")
-            os.system(f"python python/convert_to_xyz.py {input_filename} {xyzw_filename} {Omega_m} {Omega_k} {w_dark_energy} {FKP_weight} | tee -a {logfile}")
+            exec_print_and_log(f"python python/convert_to_xyz.py {input_filename} {xyzw_filename} {Omega_m} {Omega_k} {w_dark_energy} {FKP_weight}")
             input_filename = xyzw_filename
         if create_jackknives:
             xyzwj_filename = change_extension(input_filename, "xyzwj")
-            os.system(f"python python/create_jackknives_pycorr.py {data_ref_filename} {input_filename} {xyzwj_filename} {njack} | tee -a {logfile}")
+            exec_print_and_log(f"python python/create_jackknives_pycorr.py {data_ref_filename} {input_filename} {xyzwj_filename} {njack}")
             input_filename = xyzwj_filename
     # run code
     this_outdir = os.path.join(outdir, str(i)) if nfiles > 1 else outdir # create output subdirectory only if processing multiple files
     this_outdir = os.path.normpath(this_outdir) + "/" # make sure there is exactly one slash in the end
-    os.system(f"{command} -in {input_filename} -output {this_outdir} 2>&1 | tee -a {logfile}")
+    exec_print_and_log(f"{command} -in {input_filename} -output {this_outdir} 2>&1")
     print_and_log(f"Finished computation {i+1} of {nfiles}")
 # end for each random file/part
 
@@ -177,7 +181,7 @@ print_and_log(datetime.now())
 # Concatenate samples
 if nfiles > 1:
     print_and_log(f"Concatenating samples")
-    os.system(f"python python/cat_subsets_of_integrals.py {nbin} {mbin} " + " ".join([f"{os.path.join(outdir, str(i))} {maxloops}" for i in range(nfiles)]) + f" {outdir} | tee -a {logfile}")
+    exec_print_and_log(f"python python/cat_subsets_of_integrals.py {nbin} {mbin} " + " ".join([f"{os.path.join(outdir, str(i))} {maxloops}" for i in range(nfiles)]) + f" {outdir}")
 
 # Maybe post-processing will be here later
 
