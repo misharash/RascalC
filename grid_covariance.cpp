716021dcfe42cba6c4161c7dd37c5d5039a3f1aa
Michael Rashkovetskyi <misharash@gmail.com>
2022-10-07 14:57:25 -0400

    Fixed correction function rescaling

--- grid_covariance.cpp
+++ grid_covariance.cpp
@@ -196,7 +196,7 @@ int main(int argc, char *argv[]) {
         all_weights[2].rescale(all_grid[0].norm,all_grid[1].norm);
     }
 #else
-    // Now rescale correction functions based on number of particles - in the same fasion as with RR counts aboveß
+    // Now rescale correction functions based on number of particles - similar to RR counts above
     all_survey[0].rescale(all_grid[0].norm,all_grid[0].norm);
     if(par.multi_tracers==true){
         all_survey[1].rescale(all_grid[1].norm,all_grid[1].norm);
--- modules/legendre_utilities.h
+++ modules/legendre_utilities.h
@@ -115,12 +115,10 @@ public:
     }
     
     void rescale(Float norm1, Float norm2){
-        // Rescale the survey correction function by a factor (N_gal1/N_rand1)*(N_gal2/N_rand2)
+        // Rescale the survey correction function by a factor (N_rand_1/N_gal_1)*(N_rand2/N_gal2) - inverse of RR counts rescaling
         Float rescale_factor = norm1*norm2;
-        printf("Rescaling survey correction function by a factor (N_gal_1/N_rand_1)*(N_gal2/N_rand2) = %.1e\n",1./rescale_factor);
-        for(int i=0;i<nbin*n_param;i++){
-            phi_coeffs[i]/=rescale_factor;
-        }
+        printf("Rescaling survey correction function by a factor (N_rand_1/N_gal_1)*(N_rand2/N_gal2) = %.1e\n", rescale_factor);
+        for(int i = 0; i < nbin * n_param; i++) phi_coeffs[i] *= rescale_factor;
     }
     
     // Empty operator
