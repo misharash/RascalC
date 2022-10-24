d89b61186cb89e226b5352a127349890ae85505f
Michael Rashkovetskyi <misharash@gmail.com>
2022-10-24 14:12:40 -0400

    Switched to correct Antoine's xi measurements

--- run_cov.py
+++ run_cov.py
@@ -84,7 +84,7 @@ if convert_cf:
     # first index is correlation function index
     counts_factor = nrandoms
     split_above = 20
-    pycorr_filenames = [[check_path(f"/global/cfs/cdirs/desi/cosmosim/KP45/MC/Clustering/AbacusSummit/CubicBox/ELG/Xi/Pre/lhior/npy/Xi_AbacusSummit_base_c000_ph{i:03d}.npy") for i in range(25)]]
+    pycorr_filenames = [[check_path(f"/global/cfs/cdirs/desi/users/arocher/mock_challenge_ELG/v1_high_density/HOD_0/z1.1/xi/allcounts_AbacusSummit_base_c000_ph{i:03d}_HOD_0.npy", fallback_dir="antoine") for i in range(25)]]
     assert len(pycorr_filenames) == ncorr, "Expected pycorr file(s) for each correlation"
 smoothen_cf = 0
 if smoothen_cf:
@@ -108,7 +108,7 @@ nfiles = [len(input_filenames_group) for input_filenames_group in input_filename
 if not cat_randoms or make_randoms:
     for i in range(1, ntracers):
         assert nfiles[i] == nfiles[0], "Need to have the same number of files for all tracers"
-outdir = "original-HOD" # output file directory
+outdir = "antoine-HOD0" # output file directory
 tmpdir = outdir # directory to write intermediate files, mainly data processing steps
 cornames = [os.path.join(tmpdir, f"xi/xi_n{nbin_cf}_m{mbin_cf}_{index}.dat") for index in indices_corr]
 binned_pair_names = [os.path.join(tmpdir, "weights/" + ("binned_pair" if jackknife else "RR") + f"_counts_n{nbin}_m{mbin}" + (f"_j{njack}" if jackknife else "") + f"_{index}.dat") for index in indices_corr]
