ee6cb9a111f5afbdb2a554f9144c2a82a9e53f54
Michael Rashkovetskyi <misharash@gmail.com>
2023-03-13 17:37:48 -0400

    Set output dir

--- run_cov.py
+++ run_cov.py
@@ -117,7 +117,7 @@ nfiles = [len(input_filenames_group) for input_filenames_group in input_filename
 if not cat_randoms or make_randoms:
     for i in range(1, ntracers):
         assert nfiles[i] == nfiles[0], "Need to have the same number of files for all tracers"
-outdir = "_".join(tlabels) + "_" + reg # output file directory
+outdir = "unblinded_" + "_".join(tlabels) + "_" + reg # output file directory
 tmpdir = outdir # directory to write intermediate files, mainly data processing steps
 cornames = [os.path.join(tmpdir, f"xi/xi_n{nbin_cf}_m{mbin_cf}_{index}.dat") for index in indices_corr]
 binned_pair_names = [os.path.join(tmpdir, "weights/" + ("binned_pair" if jackknife else "RR") + f"_counts_n{nbin}_m{mbin}" + (f"_j{njack}" if jackknife else "") + f"_{index}.dat") for index in indices_corr]
