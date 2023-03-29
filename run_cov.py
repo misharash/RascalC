f666a890acdb1ead3c82d081fe99ecd67d7022b3
Michael Rashkovetskyi <misharash@gmail.com>
2023-03-29 15:58:30 -0400

    Set to s,mu jackknife

--- run_cov.py
+++ run_cov.py
@@ -20,7 +20,7 @@ if ntracers > 1:
     cycle_randoms = 1
 periodic = 0 # whether to run with periodic boundary conditions (must also be set in Makefile)
 make_randoms = 0 # whether to generate randoms, only works in periodic case (cubic box)
-jackknife = 0 # whether to compute jackknife integrals (must also be set in Makefile)
+jackknife = 1 # whether to compute jackknife integrals (must also be set in Makefile)
 if jackknife:
     njack = 60 # number of jackknife regions
 legendre = 1
@@ -37,7 +37,7 @@ ndata = [None] * ntracers # number of data points for each tracer; set None to m
 rmin = 0 # minimum output cov radius in Mpc/h
 rmax = 200 # maximum output cov radius in Mpc/h
 nbin = 50 # radial bins for output cov
-mbin = 20 # angular (mu) bins for output cov; in jackknife mode number of bins for correction function instead
+mbin = 1 # angular (mu) bins for output cov; in Legendre mode number of bins for correction function instead
 rmin_cf = 0 # minimum input 2PCF radius in Mpc/h
 rmax_cf = 200 # maximum input 2PCF radius in Mpc/h
 nbin_cf = 100 # radial bins for input 2PCF
