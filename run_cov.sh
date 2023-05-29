c072272eedc366f9815c1fd555ccb788e99e9494
Michael Rashkovetskyi <misharash@gmail.com>
2023-05-29 12:08:32 -0400

    Set up for NERSC

---
+++ run_cov.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=desi
+#SBATCH --constraint=cpu
+#SBATCH --qos=regular
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=RascalC-cubic-post-LRG-HODdefault
+
+# load cosmodesi environment
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+# load GSL for the C++ code
+module load gsl
+
+# OpenMP settings
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+export OMP_NUM_THREADS=128 # should match what is set in python script
+
+# Hopefully let numpy use all threads
+export NUMEXPR_MAX_THREADS=128
+
+srun python -u run_cov.py
