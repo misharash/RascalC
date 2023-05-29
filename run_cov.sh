328f0e4d04d7bfefafbcae51de2184cb8d70d67a
Michael Rashkovetskyi <misharash@gmail.com>
2023-05-29 12:17:55 -0400

    Reduced requested time, was over limit

--- run_cov.sh
+++ run_cov.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=desi
 #SBATCH --constraint=cpu
 #SBATCH --qos=regular
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=RascalC-cubic-post-LRG-HODdefault
