09b4ba0b2b3e5682c344f3c6badc25e0d8315858
Michael Rashkovetskyi <misharash@gmail.com>
2023-06-22 18:27:27 +0300

    Added full z range LRG and ELG

--- run_cov.sh
+++ run_cov.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=RascalC-Y1-blinded-recon-batch
-#SBATCH --array=0-13
+#SBATCH --array=0-17
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
