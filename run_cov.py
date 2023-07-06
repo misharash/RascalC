d6c558da02c462476c25c1559dc93e4d9d3f287d
Michael Rashkovetskyi <misharash@gmail.com>
2023-07-06 13:48:00 +0300

    Added loop number tweaks

--- run_cov.py
+++ run_cov.py
@@ -69,8 +69,11 @@ tracer1_corr, tracer2_corr = tracer1_corr_all[:ncorr], tracer2_corr_all[:ncorr]
 
 id = int(sys.argv[1]) # SLURM_JOB_ID to decide what this one has to do
 reg = "NGC" if id%2 else "SGC" # region for filenames
+# known cases where more loops are needed consistently
+if id == 0: maxloops *= 2
+elif id == 2: maxloops //= 2; maxloops *= 3 # 1.5x but stay integer
 
-id //= 2 # extracted NGC/SGC from parity, move on
+id //= 2 # extracted all needed info from parity, move on
 tracers = ['LRG'] * 3 + ['ELG_LOPnotqso'] * 2 + ['BGS_BRIGHT-21.5', 'QSO', 'LRG', 'ELG_LOPnotqso']
 zs = [[0.4, 0.6], [0.6, 0.8], [0.8, 1.1], [0.8, 1.1], [1.1, 1.6], [0.1, 0.4], [0.8, 2.1], [0.4, 1.1], [0.8, 1.6]]
 sms = [10] * 5 + [15] * 2 + [10] * 2
