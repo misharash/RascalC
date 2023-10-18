77bbbf6d239a4d5452c27dbaab023c63f96a9190
Michael Rashkovetskyi <misharash@gmail.com>
2023-10-18 17:08:13 -0400

    Launching script for the xi computation

---
+++ run_xi_jack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --account=desi_g
+#SBATCH --constraint=gpu
+#SBATCH --qos=regular
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --gpus=4
+#SBATCH --job-name=BOSS-CMASS-N-xi-jack
+
+# load cosmodesi environment
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+srun python run_xi_jack.py
