5c140d3512d03e361a22cc52c681095e1f8370a6
Michael Rashkovetskyi <misharash@gmail.com>
2023-10-18 17:03:29 -0400

    Custom 2PCF computation script

---
+++ run_xi_jack.py
@@ -0,0 +1,64 @@
+# This is the script to compute the 2PCF with jackknives
+
+import os
+import numpy as np
+from pycorr import TwoPointCorrelationFunction
+
+z_min, z_max = 0.43, 0.7
+
+work_dir = f"CMASS_N_data_{z_min}_{z_max}"
+
+data_filename = os.path.join(work_dir, "CMASS_N_data.dat.xyzwj")
+random_filename = os.path.join(work_dir, "CMASS_N_data.ran.xyzwj")
+
+# read data
+all_data = np.loadtxt(data_filename)
+n_data, no_columns = all_data.shape
+assert(no_columns == 5, "Unexpected nuber of columns in data")
+# first index is number of points, second is column: 0-2 for positions, 3 for weights, 4 for jackknife region
+
+# read randoms
+all_randoms_raw = np.loadtxt(random_filename)
+n_randoms, no_columns = all_randoms_raw.shape
+assert(no_columns == 5, "Unexpected nuber of columns in randoms")
+
+# decide number of splits so that random parts are about the data size
+n_splits = n_randoms // n_data
+
+# get the desired weight ratio
+sum_w_data = sum(all_data[:, 3])
+sum_w_randoms = sum(all_randoms_raw[:, 3])
+goal_w_ratio = sum_w_randoms / sum_w_data / n_splits
+
+# shuffle and split randoms
+np.random.shuffle(all_randoms_raw) # in place, by first axis
+all_randoms = (all_randoms_raw[i::n_splits] for i in n_splits)
+
+# reweigh each part so that the ratio of randoms to data is the same, just in case
+for i_random, random_part in enumerate(all_randoms):
+    sum_w_random_part = sum(random_part[:, 3])
+    w_ratio = sum_w_data / sum_w_random_part
+    all_randoms[i_random][:, 3] *= w_ratio / goal_w_ratio
+
+# tuple of edges, so that non-split randoms are below 20 and above they are split
+all_edges = ((s_edges, np.linspace(-1, 1, 201)) for s_edges in (np.arange(21), np.arange(20, 201)))
+
+results = []
+# compute
+for i_split_randoms, edges in enumerate(all_edges):
+    result = 0
+    D1D2 = None
+    for i_random in range(n_splits if i_split_randoms else 1):
+        these_randoms = all_randoms[i_random] if i_split_randoms else np.concatenate(all_randoms, axis=0)
+        tmp = TwoPointCorrelationFunction(mode='smu', edges=edges,
+                                          data_positions1=all_data[:, :3], data_weights1=all_data[:, 3], data_samples1=all_data[:, 4],
+                                          randoms_positions1=these_randoms[:, :3], randoms_weights1=these_randoms[:, 3], randoms_samples1=these_randoms[:, 4],
+                                          position_type='pos', engine='corrfunc', D1D2=D1D2, gpu=True, nthreads=4)
+        # position_type='pos' corresponds to (N, 3) positions shape like we have here
+        D1D2 = tmp.D1D2
+        result += tmp
+    results.append(result)
+corr = results[0].concatenate_x(*results)
+corr.D1D2.attrs['nsplits'] = n_splits
+
+corr.save(f"allcounts_BOSS_CMASS_N_{0.43}_{0.7}_lin_njack{60}_nran{n_splits}_split{20}.npy")
