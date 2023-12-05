6087ac79fd8f085b35547e3bd865443d90f25319
Michael Rashkovetskyi <misharash@gmail.com>
2023-12-05 13:41:36 -0800

    Fixed read_particles_fits_file

--- python/utils.py
+++ python/utils.py
@@ -44,7 +44,6 @@ def parse_FKP_arg(FKP_weights: str) -> bool | tuple[float, str]:
 
 def read_particles_fits_file(input_file: str, FKP_weights: bool | tuple[float, str] = False, mask: int = 0, use_weights: bool = True):
     # Read FITS file with particles. Can apply mask filtering and compute FKP weights in different ways. Works for DESI setups
-    filt = True # default pre-filter is true
     with fits.open(input_file) as f:
         data = f[1].data
         all_ra = data["RA"]
@@ -56,7 +55,9 @@ def read_particles_fits_file(input_file: str, FKP_weights: bool | tuple[float, s
             all_w *= 1/(1+FKP_weights[0]*data[FKP_weights[1]]) if FKP_weights != True else data["WEIGHT_FKP"]
         if "WEIGHT" not in colnames and not FKP_weights: warn("No weights found, assigned unit weight to each particle.")
         if mask: filt = (data["STATUS"] & mask == mask) # all 1-bits from mask have to be set in STATUS; skip if mask=0
-    return np.array((all_ra, all_dec, all_z, all_w)).T[filt]
+    result = np.array((all_ra, all_dec, all_z, all_w)).T
+    if mask: result = result[filt]
+    return result
 
 def read_xi_file(xi_file: str):
     # Interpret RascalC text format using numpy functions
