7f58bdabb1fc994c78e518c28a66971fc8f56c15
Michael Rashkovetskyi <misharash@gmail.com>
2023-12-07 15:20:32 -0500

    Changed the testing task

--- run_cov.sh
+++ run_cov.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=RascalC-Y1-v0.6.1-double-blinded-test
-#SBATCH --array=8 # only ELG 0.8-1.1 SGC for testing, not to waste allocated time
+#SBATCH --array=4 # only LRG 0.8-1.1 SGC for testing, not to waste allocated time
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
