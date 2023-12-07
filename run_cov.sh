34cb3e23c4bc5c39069a4c44918e4a3d92846408
Michael Rashkovetskyi <misharash@gmail.com>
2023-12-07 15:20:32 -0500

    One task in array for testing

--- run_cov.sh
+++ run_cov.sh
@@ -2,11 +2,11 @@
 #SBATCH --account=desi
 #SBATCH --constraint=cpu
 #SBATCH --qos=regular
-#SBATCH --time=12:00:00
+#SBATCH --time=2:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=RascalC-Y1-v0.6-double-blinded-test-debug
-#SBATCH --array=0-5 # only LRG so far
+#SBATCH --array=4 # only LRG 0.8-1.1 SGC for testing, not to waste allocated time
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
