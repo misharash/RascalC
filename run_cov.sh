b902565d6d5aaccd35f56f9346cdfd5a6ae95e49
Michael Rashkovetskyi <misharash@gmail.com>
2023-12-24 15:58:09 -0500

    Exclude the completed job id

--- run_cov.sh
+++ run_cov.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=RascalC-Y1-v0.6.1-double-blinded-test
-#SBATCH --array=0-5,8-11,16,17 # LRG and ELG without full ranges + QSO, no BGS yet
+#SBATCH --array=0-5,9-11,16,17 # LRG and ELG without full ranges + QSO, no BGS yet; exclude the completed job id 8
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
