8d9d2c0b1cb0621ec7f58c2bac07e7d4f23030fe
Michael Rashkovetskyi <misharash@gmail.com>
2023-12-26 21:12:55 -0500

    Revert "Revert "Changed task number and time limit""
    This reverts commit a6841926ac63521ab00b139b94c2a535c118a8e4.

--- run_cov.sh
+++ run_cov.sh
@@ -2,11 +2,11 @@
 #SBATCH --account=desi
 #SBATCH --constraint=cpu
 #SBATCH --qos=regular
-#SBATCH --time=2:00:00
+#SBATCH --time=4:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=RascalC-Y1-v0.6.1-double-blinded-test
-#SBATCH --array=8 # only ELG 0.8-1.1 SGC for testing, not to waste allocated time
+#SBATCH --array=4 # only LRG 0.8-1.1 SGC for testing, not to waste allocated time
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
