7d921dbb6d4bdae0595bed36d057686273562da8
Michael Rashkovetskyi <misharash@gmail.com>
2024-01-06 21:13:51 -0500

    Changed the interactive launch scheme

---
+++ run_cov2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# load cosmodesi environment
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+# GSL needed by the C++ code should already be loaded in cosmodesi
+# module load gsl
+
+# OpenMP settings
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+export OMP_NUM_THREADS=256 # should match what is set in python script
+
+# Hopefully let numpy use all threads
+export NUMEXPR_MAX_THREADS=256
+# Limit OpenBLAS thread usage (for jackknife assignment, error otherwise)
+export OPENBLAS_NUM_THREADS=1
+
+python -u run_cov.py $SLURM_ARRAY_TASK_ID
--- run_cov_interactive.sh
+++ run_cov_interactive.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-srun -N 1 -C cpu -t 00:03:00 --qos interactive python -u run_cov.py 4
+srun -N 1 -C cpu -t 00:03:00 --qos interactive bash run_cov2.sh
