965ac4d6a422e38ace22e7cd3fe35cf27c2af16c
Michael Rashkovetskyi <misharash@gmail.com>
2024-01-12 15:31:45 -0500

    Revert "Only one job while tracking NaNs"
    This reverts commit 79516eaac9ce3045d41c8b59201e86f1c3973926.

--- run_cov.sh
+++ run_cov.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=RascalC-cubic-QSO-pre
-#SBATCH --array=1 # one HOD while tracking the issue
+#SBATCH --array=1-4 # all HODs
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
