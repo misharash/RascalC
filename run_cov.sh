361b5fbe6ef3eea90d6fd6d39a43c45741520a1d
Michael Rashkovetskyi <misharash@gmail.com>
2024-01-14 15:12:10 -0500

    Revert "Launch only NGC job"
    This reverts commit 1cb1f7679b2a98a11c532d24854361fe9c79e06d.

--- run_cov.sh
+++ run_cov.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=RascalC-Y1-v0.6-blinded-LRGxELG
-#SBATCH --array=1
+#SBATCH --array=0-1
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
