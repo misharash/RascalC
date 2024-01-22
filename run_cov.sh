3ced8195c0b6721e8b6d263bd17ef12e61f9e4d8
Michael Rashkovetskyi <misharash@gmail.com>
2024-01-22 12:47:13 -0500

    Re-launch the unconverged part

--- run_cov.sh
+++ run_cov.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=RascalC-Y1-v1-blinded-recon
-#SBATCH --array=0,8 # only unconverged parts
+#SBATCH --array=8 # only unconverged part
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
