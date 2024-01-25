643f0dcfc6114abaeb4aaeea56b30ae9c852e611
Michael Rashkovetskyi <misharash@gmail.com>
2024-01-25 15:14:46 -0500

    Updated job name and selected one ID

--- run_cov.sh
+++ run_cov.sh
@@ -5,8 +5,8 @@
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --job-name=RascalC-Y1-EZmocks-recon
-#SBATCH --array=0-5,8-11,16,17 # LRG and ELG without full ranges, no BGS
+#SBATCH --job-name=RascalC-Y1-EZmocks
+#SBATCH --array=5 # LRG NGC z0.8-1.1
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
