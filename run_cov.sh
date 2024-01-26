a3b85b287ff77d202490be1cc1f97f1bf2cba442
Michael Rashkovetskyi <misharash@gmail.com>
2024-01-26 15:16:51 -0500

    Updated job name

--- run_cov.sh
+++ run_cov.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --job-name=RascalC-Y1-v0.6-blinded-LRG+ELG
+#SBATCH --job-name=RascalC-Y1-v0.6-blinded-LRG+ELG-recon
 #SBATCH --array=0-1
 
 # load cosmodesi environment
