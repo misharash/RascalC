ca6075327532131e927529de9f68767c878ea0f6
Michael Rashkovetskyi <misharash@gmail.com>
2026-02-09 13:45:50 -0500

    3PCF library docs outline

--- RascalC/__init__.py
+++ RascalC/__init__.py
@@ -3,3 +3,5 @@ __version__ = "3.1.alpha1"
 from .interface import run_cov
 from .post_process import post_process_auto
 from .raw_covariance_matrices import cat_raw_covariance_matrices
+
+from .interface_3pcf import run_cov_3pcf
