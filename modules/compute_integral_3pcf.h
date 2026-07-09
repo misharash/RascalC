198b38e007e68db24fe4ed8d9ec0269c6b9160f5
Michael Rashkovetskyi <misharash@gmail.com>
2026-07-09 07:43:53 -0700

    Tiny correction for 3PCF integral number

--- modules/compute_integral_3pcf.h
+++ modules/compute_integral_3pcf.h
@@ -322,7 +322,7 @@ class compute_integral{
             {
                 int subsample_index = completed_loops / par->loops_per_sample; // index of output subsample for this loop
                 completed_loops++; // increment completed loops counter, since they may be done not according to n_loops order
-                printf("Integral %d of %d, iteration %d of %d on thread %d completed (%d/%d)\n", iter_no, tot_iter, n_loops, par->max_loops, thread, completed_loops, par->max_loops);
+                printf("Integral %d of %d, iteration %d of %d on thread %d completed (%d/%d)\n", iter_no+1, tot_iter, n_loops, par->max_loops, thread, completed_loops, par->max_loops);
                 if (completed_loops % par->nthread == 0) { // Print every nthread completed loops
                     TotalTime.Stop(); // interrupt timing to access .Elapsed()
                     int current_runtime = TotalTime.Elapsed();
